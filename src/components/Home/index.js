cc73a96116a34e0b10ef00f1e22f91894e205c77
Nicolas Posa <nicoposa57@gmail.com>
2020-11-07 15:41:50 -0300

    creando el background del home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,6 +16,20 @@ class Home extends React.Component {
             <h2>Front End Developer</h2>
           </div>
         </div>
+        <div className="area" >
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div >
       </div>
     ) 
   }
