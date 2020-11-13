8cf367a2bf414df4c3787cac90fdb07af9453953
Nicolas Posa <nicoposa57@gmail.com>
2020-11-12 23:38:19 -0300

    agregando las imagenes de la sección skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import './index.css'
 import reactjs from '../../images/react.svg'
+import bootstrapImg from '../../images/bootstrap.svg'
+import htmlImg from '../../images/html.svg'
+import jsImg from '../../images/javascript.svg'
+import cssImg from '../../images/css3.svg'
+import typeImg from '../../images/typescript.svg'
+import gitImg from '../../images/git.svg'
+import sass from '../../images/sass.svg'
 
 class Skills extends React.Component{
   render(){
@@ -10,36 +17,36 @@ class Skills extends React.Component{
         <h1>Skills</h1>
         <div className="row fila container-sm">
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
+            <img src={htmlImg} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">HTML 5</h3>
+          </div>
+          <div className="col-5 columna">
+            <img src={jsImg} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">JavaScript</h3>
           </div>
           <div className="col-5 columna">
             <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
             <h3 className="text-center">React JS</h3>
           </div>
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
+            <img src={cssImg} className="rounded mx-auto d-block" alt=""/>
             <h3 className="text-center">React JS</h3>
           </div>
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
+            <img src={bootstrapImg} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">Bootstrap</h3>
           </div>
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
+            <img src={typeImg} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">TypeScript</h3>
           </div>
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
+            <img src={gitImg} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">Git and GitHub</h3>
           </div>
           <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
-          </div>
-          <div className="col-5 columna">
-            <img src={reactjs} className="rounded mx-auto d-block" alt=""/>
-            <h3 className="text-center">React JS</h3>
+            <img src={sass} className="rounded mx-auto d-block" alt=""/>
+            <h3 className="text-center">Sass</h3>
           </div>
         </div>
       </div>
