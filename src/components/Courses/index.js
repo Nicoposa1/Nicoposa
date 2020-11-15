2729bc9016d13a7fe19548a3e88d76525e5e7605
Nicolas Posa <nicoposa57@gmail.com>
2020-11-15 16:43:19 -0300

    terminando la seccion courses

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-import asyncImg from '../../images/asyncImg.png'
+import asyncImg from '../../images/diplomas/asyncImg.png'
+import backend from '../../images/diplomas/backend.png'
+import jspro from '../../images/diplomas/jspro.png'
+import reactesc from '../../images/diplomas/reactesc.png'
+import responsive from '../../images/diplomas/responsive.png'
+import react from '../../images/diplomas/react.png'
+import ecmascript from '../../images/diplomas/ecmascript.png'
+import redux from '../../images/diplomas/redux.png'
+import fundamentosjs from '../../images/diplomas/fundamentosjs.png'
+import fundamentosnode from '../../images/diplomas/fundamentosnode.png'
 
 import './style.css'
 
@@ -10,18 +19,29 @@ class Courses extends React.Component{
       <div className="container-fluid contenedor-courses">
         <h1 className="tittle-course">Aproved Courses</h1>
         <div className="row row-courses">
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
-                <img src={asyncImg} alt=""/>
+                <img src={jspro} alt=""/>
                 <div className="text">
-                  <h4>Curso de Asincronismo con JavaScript</h4>
+                  <h4>Curso Profesional de JavaScript</h4>
                   <p className="p-courses">Platzi-2019</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
+            <div className="card">
+              <div className="card-content">
+                <img src={reactesc} alt=""/>
+                <div className="text">
+                  <h4>Curso Práctico de React JS</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
@@ -32,45 +52,78 @@ class Courses extends React.Component{
               </div>
             </div>
           </div>
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
-                <img src={asyncImg} alt=""/>
+                <img src={responsive} alt=""/>
                 <div className="text">
-                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <h4>Curso de Responsive Design</h4>
                   <p className="p-courses">Platzi-2020</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
-                <img src={asyncImg} alt=""/>
+                <img src={backend} alt=""/>
                 <div className="text">
-                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <h4>Curso de Backend con Node</h4>
                   <p className="p-courses">Platzi-2020</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
-                <img src={asyncImg} alt=""/>
+                <img src={react} alt=""/>
                 <div className="text">
-                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <h4>Curso de React.js</h4>
                   <p className="p-courses">Platzi-2020</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-6 p-0">
+          <div className="col-6 col-md-4 p-0">
             <div className="card">
               <div className="card-content">
-                <img src={asyncImg} alt=""/>
+                <img src={fundamentosjs} alt=""/>
                 <div className="text">
-                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <h4>Fundamentos de JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4 p-0">
+            <div className="card">
+              <div className="card-content">
+                <img src={fundamentosnode} alt=""/>
+                <div className="text">
+                  <h4>Curso de Fundamentos de Node.js</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4 p-0">
+            <div className="card">
+              <div className="card-content">
+                <img src={redux} alt=""/>
+                <div className="text">
+                  <h4>Curso de React Router y Redux</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4 p-0">
+            <div className="card">
+              <div className="card-content">
+                <img src={ecmascript} alt=""/>
+                <div className="text">
+                  <h4>Curso de ECMAScript 6+</h4>
                   <p className="p-courses">Platzi-2020</p>
                 </div>
               </div>
