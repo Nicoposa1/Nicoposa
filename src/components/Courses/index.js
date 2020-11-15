8bf5edf18a7230923d13f3c13377976d8d6824eb
Nicolas Posa <nicoposa57@gmail.com>
2020-11-15 03:10:09 -0300

    terminando de modelar las cards de la sección courses

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -7,51 +7,72 @@ import './style.css'
 class Courses extends React.Component{
   render(){
     return(
-      <div className="container-fluid contenedor">
-        <h1>Aproved Courses</h1>
-        <div className="row">
-          <div className="col-6">
+      <div className="container-fluid contenedor-courses">
+        <h1 className="tittle-course">Aproved Courses</h1>
+        <div className="row row-courses">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
                 <div className="text">
-                  <h4>React</h4>
+                  <h4>Curso de Asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2019</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
+                <div className="text">
+                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
+                <div className="text">
+                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
+                <div className="text">
+                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
+                <div className="text">
+                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-0">
             <div className="card">
               <div className="card-content">
                 <img src={asyncImg} alt=""/>
+                <div className="text">
+                  <h4>Curso de asincronismo con JavaScript</h4>
+                  <p className="p-courses">Platzi-2020</p>
+                </div>
               </div>
             </div>
           </div>
